7e83f02e5c372cb3f1c528ad5b6ce743f04b4ccf
Alina Lozovskaya <alinalozov445@gmail.com>
2020-06-14 12:13:13 +0300

    main code

---
+++ WindowsFormsApp1/UserInterface.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button.Location = new System.Drawing.Point(876, 122);
+            this.button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(274, 186);
+            this.button.TabIndex = 0;
+            this.button.Text = "NEXT ITERATION (up to 8)";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(69, 80);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox.Name = "pictureBox1";
+            this.pictureBox.Size = new System.Drawing.Size(768, 578);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form";
+            this.Text = "Koch Snowflake / Koch star";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ WindowsFormsApp1/UserInterface.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        int MAX_ITER = 8;
+        int cur = -1;
+        static double X1 = 20, Y1 = 0, X2 = 20, Y2 = 100;  // start
+        public struct Vec
+        {
+
+            public double x1, y1, x2, y2;
+
+            public Vec(double x1_, double y1_, double x2_, double y2_)
+            {
+                this.x1 = x1_;
+                this.y1 = y1_;
+                this.x2 = x2_;
+                this.y2 = y2_;
+            }
+            public void turn(double alpha) // alpha - rad
+            {
+                double xn, yn;
+                xn = (x2 - x1) * Math.Cos(alpha) - (y2 - y1) * Math.Sin(alpha) + x1;
+                yn = (x2 - x1) * Math.Sin(alpha) + (y2 - y1) * Math.Cos(alpha) + y1;
+                x2 = xn;
+                y2 = yn;
+            }
+            public void go()  // (x1, y1) -> (x2, y2); (x2, y2) -> (x_new, y_new) | lenght v = const
+            {
+                double x_new, y_new;
+                x_new = x2 + (x2 - x1);
+                y_new = y2 + (y2 - y1);
+                x1 = x2;
+                y1 = y2;
+                x2 = x_new;
+                y2 = y_new;
+            }
+
+            public void to_start()
+            {
+                x1 = X1;
+                x2 = X2;
+                y1 = Y1;
+                y2 = Y2;
+            }
+        }
+
+        public Vec v = new Vec(X1, Y1, X2, Y2);
+        private void draw_line(Bitmap bmp)
+        {
+            Pen blackPen = new Pen(Color.Black, 1);
+            using (var graphics = Graphics.FromImage(bmp))
+            {
+                graphics.DrawLine(blackPen, (int) (v.x1 * 10), (int) (v.y1 * 10), (int) (v.x2 * 10), (int) (v.y2 * 10));
+                v.go();
+            }
+        }
+
+        private void turn(int alpha)  // alpha - degree
+        {
+            v.turn(Math.PI * alpha / 180.0);
+        }
+        private void line_Koch(Bitmap bmp, int n)
+        {
+            if (n == 0)
+            {
+                draw_line(bmp);
+                return;
+            }
+            line_Koch(bmp, n - 1);
+            turn(60);
+            line_Koch(bmp, n - 1);
+            turn(-120);
+            line_Koch(bmp, n - 1);
+            turn(60);
+            line_Koch(bmp, n - 1);
+        }
+
+        private void snow(Bitmap bmp, int n)
+        {
+            line_Koch(bmp, n);
+            turn(-120);
+            line_Koch(bmp, n);
+            turn(-120);
+            line_Koch(bmp, n);
+            turn(-120);
+        }
+       
+        private void draw(int n)
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            snow(bmp, n);
+            pictureBox.Image = bmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ++cur;
+            if (cur <= MAX_ITER)
+            {
+                Y2 = Y1 + (Y2 - Y1) / 3;
+                v.to_start();
+                draw(cur);
+            }
+        }
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
